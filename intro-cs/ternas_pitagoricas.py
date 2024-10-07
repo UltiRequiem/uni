f4f0e66ef6e9ea18783371ebc72c71ee481bf31a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2024-10-06 20:47:06 -0500

    quick patch

--- intro-cs/ternas_pitagoricas.py
+++ intro-cs/ternas_pitagoricas.py
@@ -1,9 +1,8 @@
-def pytagoric_pairs_up_to(number: int):
+def pytagoric_pairs_up_to(top: int):
     pairs = []
 
-    # carajo mierda, los pares los estoy generando de mayor a menor
+    for number in range(0,top):
 
-    while number > 0:
         for x in range(0,number):
             for y in range(0, number):
                 if  x>y and x**2 + y**2 == number**2:
@@ -11,20 +10,16 @@ def pytagoric_pairs_up_to(number: int):
                     is_repeated = False
 
                     for pair in pairs:
-                        print(pair)
                         if x % pair[0] == 0 and y % pair[1] == 0:
-                            print(x & pair[0])
                             is_repeated = True
                             
 
                     if not is_repeated:
                         pairs.append([x,y, number])
 
-        number-=1
-
     return pairs
 
 
-pairs_found= pytagoric_pairs_up_to(10)
+pairs_found= pytagoric_pairs_up_to(254)
 
 print(pairs_found)
