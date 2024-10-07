5cfdd260705172017514dda21c6d771b5f94374f
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2024-10-07 12:07:13 -0500

    chite 2

---
+++ intro-cs/even_sumatory.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int evenSummation(int top){
+	int result = 0;
+
+	for (int i = 0; i <= top; i++){
+		if (i % 2 == 0 ) result += i;
+	}
+
+	return result;
+}
+
+int main(){
+	int top = 0;
+
+	cout << "Hasta que numero quieres sumar los pares? ";
+	cin >> top;
+
+	cout << "El resultado es: " << evenSummation(top) << endl;
+
+	return 0;
+}
+
